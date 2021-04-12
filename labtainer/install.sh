642e92d8796fe0deae7c8291ccb540f430264a83
Osama <crowdscoops@gmail.com>
2021-04-12 11:22:00 +0100

    fix a bug

--- labtainer/install.sh
+++ labtainer/install.sh
@@ -8,8 +8,6 @@ mv ~/labtainer ~/labtainer.bak
 
 tar -xf ~/Downloads/labtainer.tar -C ~/
 
-cd ~/labtainer
-
 cp -pf install-labtainer-kali.sh ~/labtainer/
 cp -pf install-docker-kali.sh ~/labtainer/trunk/setup_scripts/install-docker-kali.sh
 
