9b5c8c1a9d4b45edd29ec60069b4653cb407df6a
Osama <crowdscoops@gmail.com>
2021-04-12 12:24:49 +0100

    add all to .doterms.sh

--- labtainer/.doterms.sh
+++ labtainer/.doterms.sh
@@ -7,3 +7,12 @@ if [[ -f /home/kali/labtainer/.dosmoke ]]; then
     gnome-terminal --geometry 120x31+150+300 --working-directory=/home/kali/labtainer/trunk/setup_scripts -e "bash -c \"exec bash -c ./full-smoke-test.sh \"" &
 fi
 
+export LABTAINER_DIR=$HOME/labtainer/trunk
+if [[ ":$PATH:" != *":./bin:"* ]]; then
+       export PATH="${PATH}:./bin:/home/kali/labtainer/trunk/scripts/designer/bin"
+fi
+
+if [[ ":$PATH:" != *":scripts/designer/bin:"* ]]; then
+   export PATH="${PATH}:/home/kali/labtainer/trunk/scripts/designer/bin"
+   export LABTAINER_DIR=/home/kali/labtainer/trunk
+fi
--- labtainer/configure-host.sh
+++ labtainer/configure-host.sh
@@ -2,13 +2,5 @@
 
 cp -p .doterms.sh /home/kali/
 echo "$HOME/.doterms.sh &" >> /home/kali/.profile
-tee -a /home/kali/.profile << END
-export LABTAINER_DIR=$HOME/labtainer/trunk
-if [[ ":$PATH:" != *":./bin:"* ]]; then
-       export PATH="${PATH}:./bin:/home/kali/labtainer/trunk/scripts/designer/bin"
-fi
-   if [[ ":$PATH:" != *":scripts/designer/bin:"* ]]; then
-       export PATH="${PATH}:/home/kali/labtainer/trunk/scripts/designer/bin"
-       export LABTAINER_DIR=/home/kali/labtainer/trunk
-   fi
-END
+#tee -a /home/kali/.profile << END
+#END
