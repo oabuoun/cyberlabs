d7258466b75c6ed73fe2b32d19993f7929f1b160
Osama <crowdscoops@gmail.com>
2021-04-13 18:54:06 +0100

    get the vms

---
+++ get_vm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd ~/Documents/Security/Kali
+
+#wget https://images.kali.org/virtual-images/kali-linux-2021.1-vmware-amd64.7z
+
+wget https://images.kali.org/virtual-images/kali-linux-2021.1-vbox-amd64.ova
