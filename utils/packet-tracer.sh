f0a1cd175f110d534e252430ed1a65dfe5af50ca
Osama <crowdscoops@gmail.com>
2021-04-13 17:56:18 +0100

    added packet tracer requirements

---
+++ utils/packet-tracer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+sudo apt-get -y install libgl1-mesa-glx
+
+sudo apt-get -y install libxcb-xinerama0-dev
+
+sudo apt-get -y install dialog
+
+#	Download PACKAGE XYZ
+#echo "=== `date` : Start Downloading PACKAGE XYZ ==="
+
+cat > ${HOME}/Desktop/PacketTracert <<EOL
+#!/usr/bin/env zsh
+
+bash /usr/local/bin/packettracer
+
+EOL
+
+chmod 755 ${HOME}/Desktop/PacketTracert
+
+EOF
+
+exit 0
