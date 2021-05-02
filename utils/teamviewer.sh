8cd1eff616eb508fd8ae7306c3873863c4f69f27
Osama <crowdscoops@gmail.com>
2021-05-02 10:21:27 +0100

    fix teamviewer downloading

--- utils/teamviewer.sh
+++ utils/teamviewer.sh
@@ -3,7 +3,7 @@
 #	Download PACKAGE XYZ
 echo "=== `date` : Start Downloading TeamViewer ==="
 
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O ~/Downloads/
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O ~/Downloads/teamviewer_amd64.deb
 
 yes | sudo dpkg -i ~/Downloads/teamviewer_amd64.deb
 
